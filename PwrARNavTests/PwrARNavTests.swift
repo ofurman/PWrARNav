fbd601a5a3cf0178d65031cba52fb7737e62f777
Oleksii <qizzup@gmail.com>
2018-10-17 14:14:40 +0200

    Added background for tests

---
+++ PwrARNavTests/PwrARNavTests.swift
@@ -0,0 +1,52 @@
+//
+//  PwrARNavTests.swift
+//  PwrARNavTests
+//
+//  Created by Oleksii Furman on 17/10/2018.
+//  Copyright © 2018 Oleksii Furman. All rights reserved.
+//
+
+import XCTest
+import CoreLocation
+import ARKit
+
+@testable import PwrARNav
+
+class PwrARNavTests: XCTestCase {
+    
+    var baseNodeUnderTest: BaseNode!
+
+    override func setUp() {
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        baseNodeUnderTest = nil
+    }
+
+    func testExample() {
+        //given
+        
+        //when
+        
+        //then
+    }
+    
+    func nodePositionSetupTest() {
+        //given
+        
+        //when
+        
+        //then
+        
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
